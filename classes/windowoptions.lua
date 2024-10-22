43ae6786c656832e6f0b326209bf323087cc640b
Your Name <you@example.com>
2024-10-23 04:05:43 +0700

    add windowoptions

---
+++ classes/windowoptions.lua
@@ -0,0 +1,52 @@
+local WindowManager = require("classes.windowmanager");
+--local Pause = require("classes.pause");
+local option = {};
+
+
+function option.ExitGame()
+	love.event.quit();
+end
+
+function option.Resume()
+	WindowManager:RemoveActiveWindow();
+	local Pause = (require("classes.pause")):Resume(); --i don't like this solution
+	--Pause:Resume()
+end
+
+function option.ToggleVisible(args)
+	args.window.visible = not args.window.visible;
+end
+
+function option.CloseAllWindows()
+	WindowManager:RemoveActiveWindow();
+end
+
+function option.OpenPreviousWindow()
+	WindowManager:BackToPreviousWindow();
+end
+
+function option.slider()
+	WindowManager:slider();
+end
+
+
+
+function option.OpenWindow(args)
+	if args.window == nil then
+		print("OpenWindow: ERROR! Expected window got nil")
+		return;
+	end
+	if args.NewWindow == nil then
+		print("Mistake. Forget set NewWindow in args for OpenWindow")
+		return;
+	end
+
+	WindowManager:SetActiveWindow(args.NewWindow);
+end
+
+
+function option.ERROR()
+	print("ERROR! ERROR in window system! options not set!");
+end
+
+return option;
